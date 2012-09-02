112a1c0753afcec1bdd02819f2f829944e1c7fd7
Carlos Rodriguez <carlos@s8f.org>
2012-09-02 16:15:56 -0700

    implement rest and test

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 var es = require('event-stream');
 
 module.exports = function () {
-  var stream = es.through()
+  var stream = es.through(write)
     , channel = 0
+    , q = []
+
+  function write (data) {
+    if (data[0] === 'rest') {
+      q.push(function (cb) {
+        setTimeout(cb, data[1]);
+      });
+    }
+    else {
+      q.push(function (cb) {
+        stream.emit('data', data);
+        cb();
+      });
+    }
+  };
+
+  function drain () {
+    if (stream.paused) {
+      return;
+    }
+    if (q.length) {
+      q.shift().call(stream, function () {
+        process.nextTick(drain);
+      });
+    }
+    else {
+      process.nextTick(drain);
+    }
+  }
+
+  stream.on('drain', drain);
 
   stream.channel = function (ch) {
     channel = ch;
@@ -12,11 +43,6 @@ module.exports = function () {
   function encodeChannel (status) {
     return status << 4 | channel;
   }
-
-  stream.channel = function (ch) {
-    channel = ch;
-    return stream;
-  };
 
   stream.send = function (status, data1, data2) {
     stream.write(fill([encodeChannel(status), data1, data2]));
@@ -33,29 +59,31 @@ module.exports = function () {
   }
 
   stream.control = function (data1, data2) {
-    stream.send(0xB, data1, data2);
-    return stream;
+    return stream.send(0xB, data1, data2);
   };
 
   stream.bank = function (number) {
-    stream.control(0, number);
-    return stream;
+    return stream.control(0, number);
   };
 
   stream.program = function (number) {
-    stream.control(0xC, number);
-    return stream;
+    return stream.control(0xC, number);
   };
 
   stream.noteOn = function (number, velocity) {
-    stream.send(0x9, number, velocity);
-    return stream;
+    return stream.send(0x9, number, velocity);
   };
 
   stream.noteOff = function (number, velocity) {
-    stream.send(0x8, number, velocity);
+    return stream.send(0x8, number, velocity);
+  };
+
+  stream.rest = function (ms) {
+    stream.write(['rest', ms]);
     return stream;
   };
+
+  drain();
 
   return stream;
 };
--- test/basic.js
+++ test/basic.js
@@ -5,11 +5,25 @@ describe('basic test', function () {
   var stream = api();
 
   it('plays', function (done) {
-    var messages = [];
+    var messages = [], restStart;
+
     stream.on('data', function (message) {
       messages.push(message);
-      if (messages.length == 4) {
-        assert.deepEqual(messages, [[ 176, 0, 0 ], [ 176, 12, 0 ], [ 144, 60, 127 ], [ 128, 60, 0 ]]);
+      if (messages.length === 3) {
+        assert.deepEqual(messages, [[ 176, 0, 0 ], [ 176, 12, 0 ], [ 144, 60, 127 ]]);
+        restStart = new Date();
+      }
+      else if (messages.length === 4) {
+        assert.deepEqual(message, [ 128, 60, 0 ]);
+        assert((new Date().getTime() - restStart.getTime()) >= 200);
+      }
+      else if (messages.length === 5) {
+        assert.deepEqual(message, [ 144, 72, 127]);
+        restStart = new Date();
+      }
+      else if (messages.length === 6) {
+        assert.deepEqual(message, [ 128, 72, 0 ]);
+        assert((new Date().getTime() - restStart.getTime()) >= 200);
         done();
       }
     });
@@ -17,6 +31,10 @@ describe('basic test', function () {
       .bank(0)
       .program(0)
       .noteOn(60, 127)
+      .rest(200)
       .noteOff(60)
+      .noteOn(72, 127)
+      .rest(200)
+      .noteOff(72)
   });
 });
